ba223bc6f9c461152af70dacbaeed4c74739a272
nikolay <nikolaybrodatskiy@gmail.com>
2024-01-01 13:20:36 +0300

    +tags vue tamplate

---
+++ resources/js/Pages/Admin/Tags/Create.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref } from "vue";
+import { useForm, Head, Link } from "@inertiajs/vue3";
+
+import AdminLayout from "@/Layouts/AdminLayout.vue";
+import PrimaryButton from "@/Components/Admin/PrimaryButton.vue";
+
+const form = useForm({
+  title: "",
+});
+
+let breadcrumbs = ref([
+  { title: "Dashboard", disabled: false, href: "dashboard.index" },
+  { title: "Categories", disabled: true, href: "categories.index" },
+]);
+</script>
+
+<template>
+  <Head title="Categories" />
+
+  <AdminLayout>
+    <v-breadcrumbs :items="breadcrumbs">
+      <template v-slot:title="{ item }">
+        <Link :href="route(item.href)">
+          {{ item.title }}
+        </Link>
+      </template>
+      <template v-slot:divider>
+        <v-icon icon="mdi-chevron-right"></v-icon>
+      </template>
+    </v-breadcrumbs>
+
+    <v-sheet class="pa-4" rounded="xl" elevation="3">
+      <div class="w-50 mx-auto">
+        <h2 class="text-center">Add new category</h2>
+        <v-form @submit.prevent="form.post(route('categories.store'))">
+          <v-text-field
+            v-model="form.title"
+            label="Title"
+            variant="underlined"
+            :error="form.errors.title ? true : false"
+            :error-messages="form.errors.title"
+          />
+
+          <div class="d-flex justify-end">
+            <PrimaryButton class="mt-2" type="submit"> Create </PrimaryButton>
+          </div>
+        </v-form>
+      </div>
+    </v-sheet>
+  </AdminLayout>
+</template>
+<style></style>
---
+++ resources/js/Pages/Admin/Tags/Index.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { ref } from "vue";
+import { Head, Link } from "@inertiajs/vue3";
+
+import AdminLayout from "@/Layouts/AdminLayout.vue";
+import PrimaryButton from "@/Components/Admin/PrimaryButton.vue";
+import DungerButton from "@/Components/Admin/DangerButton.vue";
+
+const props = defineProps(["categories"]);
+
+let breadcrumbs = ref([
+  { title: "Dashboard", disabled: false, href: "dashboard.index" },
+  { title: "Categories", disabled: true, href: "categories.index" },
+]);
+</script>
+
+<template>
+  <Head title="Categories" />
+
+  <AdminLayout>
+    <v-breadcrumbs :items="breadcrumbs">
+      <template v-slot:title="{ item }">
+        <Link :href="route(item.href)">
+          {{ item.title }}
+        </Link>
+      </template>
+      <template v-slot:divider>
+        <v-icon icon="mdi-chevron-right"></v-icon>
+      </template>
+    </v-breadcrumbs>
+
+    <v-sheet class="pa-4" rounded="xl" elevation="3">
+      <v-table fixed-header class="mt-6">
+        <thead>
+          <tr>
+            <th class="text-left">Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="category in categories" :key="category.id">
+            <td>{{ category.title }}</td>
+            <td class="text-right">
+              <Link
+                method="delete"
+                :href="route('categories.destroy', category.id)"
+              >
+                <DungerButton as="button"> Delete </DungerButton>
+              </Link>
+            </td>
+          </tr>
+        </tbody>
+      </v-table>
+      <div class="d-flex align-middle justify-center mt-4">
+        <Link :href="route('categories.create')">
+          <PrimaryButton>
+            <v-icon class="mr-4" icon="mdi-account-plus"></v-icon>
+            Add new user
+          </PrimaryButton>
+        </Link>
+      </div>
+    </v-sheet>
+  </AdminLayout>
+</template>
+<style scoped></style>
