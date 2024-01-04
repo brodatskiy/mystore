7b54613f6b91e4ce83546d9e9aa287bffebd68ce
nikolay <nikolaybrodatskiy@gmail.com>
2024-01-04 21:48:08 +0300

    remove test cards

--- resources/js/Pages/Main/Index.vue
+++ resources/js/Pages/Main/Index.vue
@@ -1,96 +1,40 @@
 <template>
-    <Head title="Main" />
+  <Head title="Main" />
 
-    <MainLayout>
-        <v-carousel hide-delimiters cycle show-arrows="hover">
-            <template v-slot:prev="{ props }">
-                <button class="" @click="props.onClick">
-                    <v-icon icon="mdi-chevron-left" size="large" />
-                </button>
-            </template>
-            <template v-slot:next="{ props }">
-                <button class="" @click="props.onClick">
-                    <v-icon icon="mdi-chevron-right" size="large" />
-                </button>
-            </template>
-            <v-carousel-item
-                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                cover
-            ></v-carousel-item>
+  <MainLayout>
+    <v-carousel hide-delimiters cycle show-arrows="hover">
+      <template v-slot:prev="{ props }">
+        <button class="" @click="props.onClick">
+          <v-icon icon="mdi-chevron-left" size="large" />
+        </button>
+      </template>
+      <template v-slot:next="{ props }">
+        <button class="" @click="props.onClick">
+          <v-icon icon="mdi-chevron-right" size="large" />
+        </button>
+      </template>
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        cover
+      ></v-carousel-item>
 
-            <v-carousel-item
-                src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
-                cover
-            ></v-carousel-item>
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+        cover
+      ></v-carousel-item>
 
-            <v-carousel-item
-                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-                cover
-            ></v-carousel-item>
-        </v-carousel>
+      <v-carousel-item
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        cover
+      ></v-carousel-item>
+    </v-carousel>
 
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="mt-24">123</div>
-        <div class="p-4 max-w-7xl mx-auto flex flex-col items-center">
-            <!-- <div class="menu flex mb-2">
+    <div class="p-4 max-w-7xl mx-auto flex flex-col items-center">
+      <!-- <div class="menu flex mb-2">
                 <FilterProducts></FilterProducts>
             </div> -->
 
-            <!-- <div
+      <!-- <div
                 v-if="!productStore.isLoading"
                 class="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mb-5"
             >
@@ -103,28 +47,24 @@
 
 
             </div> -->
-            <div
-                class="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mb-5>"
-            >
-                <ProductCard
-                    v-for="product in products"
-                    :key="product.id"
-                    :product="product"
-                />
-            </div>
-            <div
-                class="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mb-5>"
-            >
-                <ProductCard
-                    v-for="product in products"
-                    :key="product.id"
-                    :product="product"
-                />
-            </div>
+      <div class="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mb-5>">
+        <ProductCard
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+        />
+      </div>
+      <div class="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mb-5>">
+        <ProductCard
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+        />
+      </div>
 
-            <!-- <div v-else class="mb-3"><p>Loading...</p></div> -->
-        </div>
-    </MainLayout>
+      <!-- <div v-else class="mb-3"><p>Loading...</p></div> -->
+    </div>
+  </MainLayout>
 </template>
 <script setup>
 import { ref, onMounted } from "vue";
