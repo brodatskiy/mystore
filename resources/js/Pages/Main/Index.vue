2b46d6803ec8f8c55a4f30904799b6936de11262
nikolay <nikolaybrodatskiy@gmail.com>
2024-01-05 21:38:39 +0300

    v-card

--- resources/js/Pages/Main/Index.vue
+++ resources/js/Pages/Main/Index.vue
@@ -1,3 +1,22 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+// import { useProductStore } from "@/Store/useProductStore";
+import MainLayout from "@/Layouts/MainLayout.vue";
+import { Head } from "@inertiajs/vue3";
+import { Link } from "@inertiajs/vue3";
+
+import ModalWindow from "@/Components/ModalWindow.vue";
+import CreateProduct from "@/Components/CreateProduct.vue";
+import ProductCard from "@/Components/ProductCard.vue";
+// import FilterProducts from "@/Components/FilterProducts.vue";
+
+// const productStore = useProductStore();
+const props = defineProps(["products"]);
+let showModal = ref(false);
+
+onMounted(() => {});
+</script>
 <template>
   <Head title="Main" />
 
@@ -47,36 +66,20 @@
 
 
             </div> -->
-      <div class="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mb-5>">
-        <ProductCard
-          v-for="product in products"
-          :key="product.id"
-          :product="product"
-        />
-      </div>
+      <v-container>
+        <v-row>
+          <v-col v-for="product in products" :key="n" cols="4">
+            <ProductCard :product="product"></ProductCard>
+          </v-col>
+        </v-row>
+      </v-container>
+      <div
+        class="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mb-5>"
+      ></div>
 
       <!-- <div v-else class="mb-3"><p>Loading...</p></div> -->
     </div>
   </MainLayout>
 </template>
-<script setup>
-import { ref, onMounted } from "vue";
-
-// import { useProductStore } from "@/Store/useProductStore";
-import MainLayout from "@/Layouts/MainLayout.vue";
-import { Head } from "@inertiajs/vue3";
-import { Link } from "@inertiajs/vue3";
-
-import ModalWindow from "@/Components/ModalWindow.vue";
-import CreateProduct from "@/Components/CreateProduct.vue";
-import ProductCard from "@/Components/ProductCard.vue";
-// import FilterProducts from "@/Components/FilterProducts.vue";
-
-// const productStore = useProductStore();
-const props = defineProps(["products"]);
-let showModal = ref(false);
-
-onMounted(() => {});
-</script>
 
 <style scoped></style>
