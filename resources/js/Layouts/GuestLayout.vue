431f0928033abff726c04af1fe4d267594c2feeb
nikolay <nikolaybrodatskiy@gmail.com>
2024-01-22 22:27:44 +0300

    bg

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,26 +4,26 @@ import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <v-app>
-        <v-sheet color="surface" class="h-100 d-flex align-center">
-            <v-card
-                class="d-flex flex-column align-center w-25 px-6 py-4 mx-auto bg-white"
-                rounded="xl"
-                max-width="448"
-            >
-                <div>
-                    <Link :href="route('/')" class="d-flex align-baseline">
-                        <v-icon icon="mdi-store" size="large" class=""></v-icon>
-                        <h1 class="font-weight-bold">MyStore</h1>
-                    </Link>
-                </div>
+  <v-app>
+    <v-sheet color="background" class="h-100 d-flex align-center">
+      <v-card
+        class="d-flex flex-column align-center w-25 px-6 py-4 mx-auto bg-white"
+        rounded="xl"
+        max-width="448"
+      >
+        <div class="mb-3">
+          <Link :href="route('/')" class="d-flex align-baseline">
+            <v-icon icon="mdi-store" size="large" class=""></v-icon>
+            <h1 class="font-weight-bold">MyStore</h1>
+          </Link>
+        </div>
 
-                <div class="w-75 mt-2">
-                    <slot />
-                </div>
-            </v-card>
-        </v-sheet>
-    </v-app>
+        <div class="w-75 mt-2">
+          <slot />
+        </div>
+      </v-card>
+    </v-sheet>
+  </v-app>
 </template>
 
 <style></style>
