a217a4ac0eed4ec992e25603aec53a24b8476901
nikolay <nikolaybrodatskiy@gmail.com>
2024-01-23 15:46:58 +0300

    -comms

--- resources/js/Pages/Main/Index.vue
+++ resources/js/Pages/Main/Index.vue
@@ -1,53 +1,41 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
+import { useDisplay } from "vuetify";
 
-// import { useProductStore } from "@/Store/useProductStore";
 import MainLayout from "@/Layouts/MainLayout.vue";
 import { Head } from "@inertiajs/vue3";
 import { Link } from "@inertiajs/vue3";
 
-import ModalWindow from "@/Components/ModalWindow.vue";
-import CreateProduct from "@/Components/CreateProduct.vue";
 import ProductCard from "@/Components/ProductCard.vue";
-// import FilterProducts from "@/Components/FilterProducts.vue";
 
-// const productStore = useProductStore();
 const props = defineProps(["products"]);
 let showModal = ref(false);
 
-onMounted(() => {});
+const display = useDisplay();
+
+console.log(display);
+
+const cols = computed(() => {
+  const { lg, sm } = useDisplay();
+  return lg ? 3 : sm ? 4 : 6;
+});
 </script>
 <template>
   <Head title="Main" />
 
   <MainLayout>
     <div class="p-4 max-w-7xl mx-auto flex flex-col items-center">
-      <!-- <div class="menu flex mb-2">
-                <FilterProducts></FilterProducts>
-            </div> -->
-
-      <!-- <div
-                v-if="!productStore.isLoading"
-                class="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mb-5"
-            >
-                <template
-                    v-for="product in productStore.filtredAndSearchAndSortProducts"
-                    :key="product.id"
-                >
-                    <ProductCard v-if="product.id" :product="product" />
-                </template>
-
-
-            </div> -->
       <v-container>
         <v-row>
-          <v-col v-for="product in products" :key="product.id" cols="3">
+          <v-col
+            v-for="(product, index) in products"
+            :key="product.id"
+            :cols="cols"
+          >
             <ProductCard :product="product"></ProductCard>
           </v-col>
         </v-row>
       </v-container>
-
-      <!-- <div v-else class="mb-3"><p>Loading...</p></div> -->
     </div>
   </MainLayout>
 </template>
