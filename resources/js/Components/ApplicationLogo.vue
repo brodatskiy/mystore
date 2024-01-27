80e93afd8c9164356896ade24ba063873a2bed8d
nikolay <nikolaybrodatskiy@gmail.com>
2024-01-27 18:24:35 +0300

    margin/ h-text edit

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -2,7 +2,7 @@
 import { Link } from "@inertiajs/vue3";
 </script>
 <template>
-  <Link :href="route('/')" class="d-flex align-baseline justify-center mb-3">
+  <Link :href="route('/')" class="d-flex align-baseline">
     <v-icon icon="mdi-store" size="x-large" class=""></v-icon>
     <p class="font-weight-bold text-h4">MyStore</p>
   </Link>
