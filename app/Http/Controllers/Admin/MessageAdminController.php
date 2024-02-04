55c20c9d4b796f0d1dd7c260e9e93069e38b0344
nikolay <nikolaybrodatskiy@gmail.com>
2024-02-04 20:38:38 +0300

    init admin message controller

---
+++ app/Http/Controllers/Admin/MessageAdminController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\StoreRequest;
+use App\Http\Resources\Message\MessageResource;
+use App\Mail\User\PasswordMail;
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Inertia\Inertia;
+
+class MessageAdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $messages = Message::all();
+
+        return Inertia::render('Admin/Messages/Index', [
+            'messages' => MessageResource::collection($messages)
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request): RedirectResponse
+    {
+        $data = $request->validated();
+
+        if (isset($data['avatar'])) {
+            $data['avatar'] = Storage::disk('public')->put('/images', $data['avatar']);
+        }
+
+        $password = Str::random(10);
+        $data['password'] = Hash::make($password);
+
+        $user = User::firstOrCreate(['email' => $data['email']], $data);
+        Mail::to($data['email'])->send(new PasswordMail($password));
+        event(new Registered($user));
+        return to_route('users.index');
+    }
+}
