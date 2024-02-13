620516db75eaecb5eaabcbdb74195996833cb6cb
nikolay <nikolaybrodatskiy@gmail.com>
2024-02-13 16:24:00 +0300

    -users factory/ add UserSeeder and GroupSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,27 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(7)->create();
-
-        User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('admin'),
-            'role' => 1,
-        ]);
-        User::factory()->create([
-            'name' => 'user',
-            'email' => 'user@user.com',
-            'password' => Hash::make('user'),
-            'role' => 1,
-        ]);
-        User::factory()->create([
-            'name' => 'user2',
-            'email' => 'user2@user.com',
-            'password' => Hash::make('user2'),
-            'role' => 1,
-        ]);
-
+        $this->call(UserSeeder::class);
+        $this->call(GroupSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(TagSeeder::class);
         $this->call(ProductSeeder::class);
