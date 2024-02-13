cbc23854b14972edf33e637a14d732e2efca808c
nikolay <nikolaybrodatskiy@gmail.com>
2024-02-13 14:09:10 +0300

    add test users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,12 +17,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
+        User::factory(7)->create();
 
         User::factory()->create([
             'name' => 'admin',
             'email' => 'admin@admin.com',
             'password' => Hash::make('admin'),
+            'role' => 1,
+        ]);
+        User::factory()->create([
+            'name' => 'user',
+            'email' => 'user@user.com',
+            'password' => Hash::make('user'),
+            'role' => 1,
+        ]);
+        User::factory()->create([
+            'name' => 'user2',
+            'email' => 'user2@user.com',
+            'password' => Hash::make('user2'),
             'role' => 1,
         ]);
 
