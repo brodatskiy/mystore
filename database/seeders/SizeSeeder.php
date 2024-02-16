f0bac49725403af5a27e5669a5a2ba5270db7dd5
nikolay <nikolaybrodatskiy@gmail.com>
2024-02-16 20:35:32 +0300

    init

---
+++ database/seeders/SizeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ProductSize;
+use App\Models\Size;
+use Illuminate\Database\Seeder;
+
+class SizeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Size::factory()->create(['title' => 'xl']);
+        Size::factory()->create(['title' => 's']);
+        Size::factory()->create(['title' => 'm']);
+        Size::factory()->create(['title' => 'l']);
+        Size::factory()->create(['title' => 'xl']);
+
+        ProductSize::factory(10)->create();
+    }
+}
