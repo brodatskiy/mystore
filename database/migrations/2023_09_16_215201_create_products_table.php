da7e62672ccaa42a3e21bcca23a5434aa65b4c3a
nikolay <nikolaybrodatskiy@gmail.com>
2024-02-16 19:37:55 +0300

    -count

--- database/migrations/2023_09_16_215201_create_products_table.php
+++ database/migrations/2023_09_16_215201_create_products_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->text('description');
             $table->string('preview_image');
             $table->integer('price');
-            $table->integer('count');
             $table->boolean('is_published')->default(true);
             $table->foreignId('category_id')->index()->constrained('categories')->onDelete('cascade');
 
