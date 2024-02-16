aa6ef2ecdbe1252dcc313e083dcddbf065f622a1
nikolay <nikolaybrodatskiy@gmail.com>
2024-02-16 20:12:25 +0300

    -count

--- database/migrations/2024_02_16_162825_create_sizes_table.php
+++ database/migrations/2024_02_16_162825_create_sizes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('sizes', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('count');
+
             $table->timestamps();
         });
     }
