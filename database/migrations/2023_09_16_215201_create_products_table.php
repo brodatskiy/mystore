2dfb01e7ab60cfba5b5ba1064ff96d3ed999d517
nikolay <nikolaybrodatskiy@gmail.com>
2024-02-16 19:10:24 +0300

    -content column

--- database/migrations/2023_09_16_215201_create_products_table.php
+++ database/migrations/2023_09_16_215201_create_products_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->text('content');
             $table->string('preview_image');
             $table->integer('price');
             $table->integer('count');
