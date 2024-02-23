80266dc7d25f9b0d498abcc6ea95d5f1dc17e7ae
nikolay <nikolaybrodatskiy@gmail.com>
2024-02-23 16:28:46 +0300

    fix grids

--- resources/js/Pages/Client/Home/Index.vue
+++ resources/js/Pages/Client/Home/Index.vue
@@ -3,15 +3,14 @@ import { ref, onMounted, computed } from "vue";
 import { useDisplay } from "vuetify";
 
 import MainLayout from "@/Layouts/MainLayout.vue";
-import { Head } from "@inertiajs/vue3";
-import { Link } from "@inertiajs/vue3";
+import { Head, Link } from "@inertiajs/vue3";
 
 import ProductCard from "@/Components/ProductCard.vue";
 
 const props = defineProps(["products"]);
+const { name } = useDisplay();
 
 const cols = computed(() => {
-  const { name } = useDisplay();
   switch (name.value) {
     case "md":
       return 4;
@@ -30,12 +29,7 @@ const cols = computed(() => {
   <MainLayout>
     <v-container class="pa-0 grid-container">
       <v-row>
-        <v-col
-          v-for="product in products"
-          :key="product.id"
-          :cols="cols"
-          class="px-1"
-        >
+        <v-col v-for="product in products" :key="product.id" :cols="cols">
           <ProductCard :product="product"></ProductCard>
         </v-col>
       </v-row>
