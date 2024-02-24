78e11b82e3d9871fb5f1ee54c8fa28a7f01edd5b
nikolay <nikolaybrodatskiy@gmail.com>
2024-02-24 17:48:11 +0300

    add Link\ edit

--- resources/js/Components/ProductCard.vue
+++ resources/js/Components/ProductCard.vue
@@ -8,24 +8,31 @@ import { Link } from "@inertiajs/vue3";
 defineProps(["product"]);
 </script>
 <template>
-  <v-hover v-slot="{ isHovering, props }">
-    <v-card class="product" variant="text" v-bind="props">
-      <div class="product__image-block">
-        <v-img :src="product.image" alt="Card Image" cover aspect-ratio="1.4" />
-        <div class="product_sticker px-1">
-          {{ product.sticker.title.toUpperCase() }}
+  <Link :href="route('product.show', product)">
+    <v-hover v-slot="{ isHovering, props }">
+      <v-card class="product" variant="text" v-bind="props">
+        <div class="product__image-block">
+          <v-img
+            :src="product.image"
+            alt="Card Image"
+            cover
+            aspect-ratio="1.4"
+          />
+          <div class="product_sticker px-1">
+            {{ product.sticker.title.toUpperCase() }}
+          </div>
         </div>
-      </div>
-      <div class="d-flex justify-space-between">
-        <div>{{ product.price }} $</div>
-        <div>{{ product.title }}</div>
-        <div>
-          <v-icon icon="mdi-heart-outline" size="small" />
-          <!-- <v-icon icon="mdi-heart" size="small" /> -->
+        <div class="d-flex justify-space-between">
+          <div>{{ product.price }} $</div>
+          <div>{{ product.title }}</div>
+          <div>
+            <v-icon icon="mdi-heart-outline" size="small" />
+            <!-- <v-icon icon="mdi-heart" size="small" /> -->
+          </div>
         </div>
-      </div>
-    </v-card>
-  </v-hover>
+      </v-card>
+    </v-hover>
+  </Link>
 </template>
 <style scoped>
 .product:hover .product__bottom {
@@ -39,10 +46,6 @@ defineProps(["product"]);
   position: relative;
   width: 100%;
   height: auto;
-}
-.product_stickers {
-  position: absolute;
-  bottom: 0;
 }
 .product_sticker {
   position: absolute;
