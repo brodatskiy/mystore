27ff5063aa187540d63949b5b3d8388303069cf5
nikolay <nikolaybrodatskiy@gmail.com>
2024-02-26 16:12:35 +0300

    init

---
+++ app/Models/Image.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Image extends Model
+{
+    protected $table = 'images';
+    protected $guarded = false;
+
+    public function imageable()
+    {
+        return $this->morphTo();
+    }
+}
