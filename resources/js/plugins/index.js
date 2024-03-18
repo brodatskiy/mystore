ed254c7a4f3e1d05cd214c4c1dbe115fda22a8fa
nikolay <nikolaybrodatskiy@gmail.com>
2024-03-18 20:55:57 +0300

    add PrimeVue presets

--- resources/js/plugins/index.js
+++ resources/js/plugins/index.js
@@ -3,11 +3,15 @@
  *
  */
 
+import PrimeVue from "primevue/config";
+import Lara from "~/presets/Lara";
+
 // Plugins
-import vuetify from "./vuetify";
+// import vuetify from "./vuetify";
 // import pinia from '../store'
 // import router from '../router'
 
 export function registerPlugins(app) {
-    app.use(vuetify);
+  app.use(PrimeVue, { unstyled: true, pt: Lara });
+  // app.use(vuetify);
 }
