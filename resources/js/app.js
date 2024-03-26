dab07a6e15a88ad83793f1e3d15c850cc3f59b2b
Brodatsky <brokoli133750@gmail.com>
2024-03-26 15:30:11 +0300

    edit

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,7 @@ import "../css/app.css";
 import { createApp, h } from "vue";
 import { createInertiaApp, Link, Head } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-
-import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
 import { Icon } from "@iconify/vue";
 
 // Plugins
@@ -14,23 +13,23 @@ import { registerPlugins } from "@/plugins";
 const appName = import.meta.env.VITE_APP_NAME || "MyStore";
 
 createInertiaApp({
-  title: (title) => `${title} - ${appName}`,
-  resolve: (name) =>
-    resolvePageComponent(
-      `./Pages/${name}.vue`,
-      import.meta.glob("./Pages/**/*.vue")
-    ),
-  setup({ el, App, props, plugin }) {
-    return createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .use(ZiggyVue, Ziggy)
-      .component("Link", Link)
-      .component("Head", Head)
-      .component("Icon", Icon)
-      .use(registerPlugins)
-      .mount(el);
-  },
-  progress: {
-    color: "#4B5563",
-  },
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .component("Link", Link)
+            .component("Head", Head)
+            .component("Icon", Icon)
+            .use(registerPlugins)
+            .mount(el);
+    },
+    progress: {
+        color: "#4B5563",
+    },
 });
