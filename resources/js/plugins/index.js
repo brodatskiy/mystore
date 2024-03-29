20c23fb265b31f48d42d42731c1eb4fbe8b7899e
Brodatsky <brokoli133750@gmail.com>
2024-03-29 16:00:53 +0300

    add toast

--- resources/js/plugins/index.js
+++ resources/js/plugins/index.js
@@ -4,11 +4,12 @@
  */
 
 import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
 import Lara from "~/presets/Lara";
 
 // Plugins
 // import pinia from '../store'
 
 export function registerPlugins(app) {
-    app.use(PrimeVue, { unstyled: true, pt: Lara });
+    app.use(PrimeVue, { unstyled: true, pt: Lara }).use(ToastService);
 }
