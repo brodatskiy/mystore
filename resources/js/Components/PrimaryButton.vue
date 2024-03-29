daf92ebada9ae0f74a930fba8451823c2f1095b9
Brodatsky <brokoli133750@gmail.com>
2024-03-29 16:01:48 +0300

    remove

--- resources/js/Components/PrimaryButton.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-
-<template>
-  <v-btn
-    class="v-btn v-btn--flat v-theme--myCustomLightTheme bg-primary v-btn--density-default v-btn--size-default v-btn--variant-elevated rounded-xl"
-  >
-    <slot />
-  </v-btn>
-</template>
