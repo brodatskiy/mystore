69674ceb71512d50ba4ad0ef121145983a257951
Brodatsky <brokoli133750@gmail.com>
2024-03-29 16:01:38 +0300

    rename

--- resources/js/Components/Button.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <button
-    class="px-4 py-2 font-sans text-xs font-bold text-center bg-gray-100 text-gray-900 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:bg-gray-900/15 active:bg-gray-900/30"
-  >
-    <slot />
-  </button>
-</template>
