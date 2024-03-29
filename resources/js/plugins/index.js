f5f62e2f25a5b019ee6eaa5bf3cb9ffb62bd6179
Brodatsky <brokoli133750@gmail.com>
2024-03-29 12:10:59 +0300

    -comms

--- resources/js/plugins/index.js
+++ resources/js/plugins/index.js
@@ -7,11 +7,8 @@ import PrimeVue from "primevue/config";
 import Lara from "~/presets/Lara";
 
 // Plugins
-// import vuetify from "./vuetify";
 // import pinia from '../store'
-// import router from '../router'
 
 export function registerPlugins(app) {
-  app.use(PrimeVue, { unstyled: true, pt: Lara });
-  // app.use(vuetify);
+    app.use(PrimeVue, { unstyled: true, pt: Lara });
 }
