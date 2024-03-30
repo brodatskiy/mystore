9affe7e221b1e04e36d21717940eb5f1c08b80da
Brodatsky <brokoli133750@gmail.com>
2024-03-30 21:29:38 +0300

    pv rework

--- resources/js/Pages/Client/Profile/Edit.vue
+++ resources/js/Pages/Client/Profile/Edit.vue
@@ -3,79 +3,122 @@ import { ref } from "vue";
 import { useForm, usePage } from "@inertiajs/vue3";
 
 import ShopLayout from "@/Layouts/ShopLayout.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
+import Btn from "@/Components/Btn.vue";
+
+import InputNumber from "primevue/inputnumber";
+import InputError from "@/Components/InputError.vue";
 
 let props = defineProps({
-  mustVerifyEmail: Boolean,
-  status: String,
+    mustVerifyEmail: Boolean,
+    status: String,
 });
 
 const user = usePage().props.auth.user;
 
 const form = useForm({
-  _method: "patch",
-  name: user.name,
-  email: user.email,
-  age: user.age,
-  address: user.address,
-  gender: user.gender,
-  role: user.role,
-  avatar: "",
+    _method: "patch",
+    name: user.name,
+    email: user.email,
+    age: user.age,
+    address: user.address,
+    gender: user.gender,
+    role: user.role,
+    avatar: "",
 });
 
 const genderItems = ref([
-  { value: 1, title: "Male" },
-  { value: 2, title: "Female" },
+    { value: 1, title: "Male" },
+    { value: 2, title: "Female" },
 ]);
 </script>
 
 <template>
-  <Head title="Profile" />
+    <Head title="Profile" />
 
-  <ShopLayout>
-    <v-sheet class="pa-4" rounded="xl" elevation="3">
-      <div class="w-50 mx-auto">
-        <h2 class="text-center mt-2">Profile</h2>
-        <form @submit.prevent="form.post(route('profile.update', user.id))">
-          <v-text-field
-            v-model="form.name"
-            label="Name"
-            variant="underlined"
-            :error="form.errors.name ? true : false"
-            :error-messages="form.errors.name"
-          />
-          <v-text-field
-            v-model="form.email"
-            label="Email"
-            variant="underlined"
-            :error="form.errors.email ? true : false"
-            :error-messages="form.errors.email"
-          />
-          <v-text-field v-model="form.age" label="Age" variant="underlined" />
-          <v-text-field
-            v-model="form.address"
-            label="Address"
-            variant="underlined"
-          />
-          <v-select
-            v-model="form.gender"
-            item-value="value"
-            variant="underlined"
-            label="Gender"
-            :items="genderItems"
-          ></v-select>
-          <v-file-input
-            @input="form.avatar = $event.target.files[0]"
-            clearable
-            variant="underlined"
-            label="Choose avatar"
-          ></v-file-input>
-          <div class="d-flex justify-end">
-            <PrimaryButton class="mt-2" type="submit"> Update </PrimaryButton>
-          </div>
-        </form>
-      </div>
-    </v-sheet>
-  </ShopLayout>
+    <ShopLayout>
+        <div
+            class="flex flex-col items-center px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+        >
+            <h2 class="text-center mt-2">Profile</h2>
+            <div class="w-1/2 pa-4 mt-2">
+                <form
+                    @submit.prevent="
+                        form.post(route('profile.update', user.id))
+                    "
+                >
+                    <div class="flex flex-col items-center gap-3">
+                        <div class="flex flex-col gap-1">
+                            <label for="name">Username</label>
+                            <InputText
+                                v-model="form.name"
+                                type="name"
+                                id="name"
+                                autofocus
+                                class="w-[17rem]"
+                            />
+                            <InputError :message="form.errors.name" />
+                        </div>
+
+                        <div class="flex flex-col gap-1">
+                            <label for="email">Email</label>
+                            <InputText
+                                v-model="form.email"
+                                type="email"
+                                id="email"
+                                class="w-[17rem]"
+                            />
+                            <InputError :message="form.errors.email" />
+                        </div>
+
+                        <div class="flex flex-col gap-1">
+                            <label for="age">Age</label>
+                            <InputNumber
+                                v-model="form.age"
+                                inputId="age"
+                                :useGrouping="false"
+                                class="w-[17rem]"
+                            />
+                            <InputError :message="form.errors.age" />
+                        </div>
+
+                        <div class="flex flex-col gap-1">
+                            <label for="address">Address</label>
+                            <InputText
+                                v-model="form.address"
+                                class="w-[17rem]"
+                            />
+                            <InputError :message="form.errors.address" />
+                        </div>
+
+                        <div class="flex flex-col gap-1">
+                            <label for="name">Gender</label>
+                            <Dropdown
+                                v-model="form.gender"
+                                :options="genderItems"
+                                optionLabel="title"
+                                optionValue="value"
+                                placeholder="Select a gender"
+                                class="w-[17rem]"
+                            />
+                            <InputError :message="form.errors.gender" />
+                        </div>
+
+                        <div class="flex flex-col gap-1">
+                            <label for="avatar">Upload avatar</label>
+                            <FileUpload
+                                mode="basic"
+                                class="mt-1"
+                                @input="form.avatar = $event.target.files[0]"
+                            />
+
+                            <Button class="font-bold mt-2" type="submit">
+                                Update
+                            </Button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </ShopLayout>
 </template>
 <style></style>
