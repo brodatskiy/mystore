6b6443ae7d31f41a2d26ea5e7e38f89033ffba26
Brodatsky <brokoli133750@gmail.com>
2024-03-30 21:28:44 +0300

    edit

--- resources/js/Components/LinkBtn.vue
+++ resources/js/Components/LinkBtn.vue
@@ -1,8 +1,8 @@
 <template>
-  <Link
-    v-bind="$attrs"
-    class="px-4 py-2 font-sans text-xs font-bold text-center bg-gray-100 text-gray-900 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:bg-gray-900/15 active:bg-gray-900/30"
-  >
-    <slot />
-  </Link>
+    <Link
+        v-bind="$attrs"
+        class="px-4 py-2 font-sans text-s font-bold text-center bg-gray-100 text-gray-900 align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:bg-gray-900/15 active:bg-gray-900/30"
+    >
+        <slot />
+    </Link>
 </template>
