95602052b242d399e33ee58ea98ffbabfc027bc3
Brodatsky <brokoli133750@gmail.com>
2024-04-07 18:27:13 +0300

    remove comms

--- app/Console/Commands/InstallCommand.php
+++ app/Console/Commands/InstallCommand.php
@@ -6,23 +6,11 @@ use Illuminate\Console\Command;
 
 class InstallCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+
     protected $signature = 'my-store:install';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Installation';
 
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
         $this->call('storage:link');
--- app/Console/Commands/RefreshCommand.php
+++ app/Console/Commands/RefreshCommand.php
@@ -7,23 +7,11 @@ use Illuminate\Support\Facades\Storage;
 
 class RefreshCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+
     protected $signature = 'my-store:refresh';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Refresh';
 
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
         if (app()->isProduction()) {
