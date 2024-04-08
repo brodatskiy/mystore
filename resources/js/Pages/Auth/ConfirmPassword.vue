fce8dd6ad16975172ab1acccf3d8ea46a9453c21
Brodatsky <brokoli133750@gmail.com>
2024-04-08 18:37:37 +0300

    formmater

--- resources/js/Pages/Auth/ConfirmPassword.vue
+++ resources/js/Pages/Auth/ConfirmPassword.vue
@@ -1,53 +1,53 @@
 <script setup>
 import AuthLayout from "@/Layouts/AuthLayout.vue";
-import { Head, useForm } from "@inertiajs/vue3";
+import { useForm } from "@inertiajs/vue3";
 
 const form = useForm({
-  password: "",
+    password: "",
 });
 
 const submit = () => {
-  form.post(route("password.confirm"), {
-    onFinish: () => form.reset(),
-  });
+    form.post(route("password.confirm"), {
+        onFinish: () => form.reset(),
+    });
 };
 </script>
 
 <template>
-  <AuthLayout>
-    <Head title="Confirm Password" />
+    <AuthLayout>
+        <Head title="Confirm Password" />
 
-    <div class="mb-4">
-      This is a secure area of the application. Please confirm your password
-      before continuing.
-    </div>
+        <div class="mb-4">
+            This is a secure area of the application. Please confirm your
+            password before continuing.
+        </div>
 
-    <v-form @submit.prevent="submit">
-      <v-text-field
-        v-model="form.password"
-        :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="showPassword ? 'text' : 'password'"
-        density="compact"
-        placeholder="Enter your password"
-        prepend-inner-icon="mdi-lock-outline"
-        variant="underlined"
-        label="Password"
-        :error="form.errors.password ? true : false"
-        :error-messages="form.errors.password"
-        @click:append-inner="showPassword = !showPassword"
-      ></v-text-field>
+        <v-form @submit.prevent="submit">
+            <v-text-field
+                v-model="form.password"
+                :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+                :type="showPassword ? 'text' : 'password'"
+                density="compact"
+                placeholder="Enter your password"
+                prepend-inner-icon="mdi-lock-outline"
+                variant="underlined"
+                label="Password"
+                :error="form.errors.password ? true : false"
+                :error-messages="form.errors.password"
+                @click:append-inner="showPassword = !showPassword"
+            ></v-text-field>
 
-      <v-btn
-        block
-        type="submit"
-        :class="{ 'opacity-25': form.processing }"
-        :disabled="form.processing"
-        color="primary"
-        size="large"
-        variant="flat"
-      >
-        Confirm
-      </v-btn>
-    </v-form>
-  </AuthLayout>
+            <v-btn
+                block
+                type="submit"
+                :class="{ 'opacity-25': form.processing }"
+                :disabled="form.processing"
+                color="primary"
+                size="large"
+                variant="flat"
+            >
+                Confirm
+            </v-btn>
+        </v-form>
+    </AuthLayout>
 </template>
