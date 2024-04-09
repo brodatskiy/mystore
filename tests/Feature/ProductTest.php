c96e3b463284b1ccad0d76eed4cf153d1c739dbb
Brodatsky <brokoli133750@gmail.com>
2024-04-09 20:47:31 +0300

    init

---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_product_page_is_displayed(): void
+    {
+
+        $product = Product::factory()->create();
+
+        // $response = $this->get(route('product.show', [
+        //     'id' => $product->id,
+        // ]));
+
+
+        $response = $this->get('product/' . $product->id);
+
+        $response->assertStatus(200);
+    }
+}
