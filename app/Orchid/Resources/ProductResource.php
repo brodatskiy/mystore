e548cbc7900e7623432073f5e12bd3fc02737e0d
Brodatsky <brokoli133750@gmail.com>
2024-04-30 19:01:18 +0300

    TD

--- app/Orchid/Resources/ProductResource.php
+++ app/Orchid/Resources/ProductResource.php
@@ -2,8 +2,16 @@
 
 namespace App\Orchid\Resources;
 
+use App\Models\Category;
+use Orchid\Crud\Filters\DefaultSorted;
 use Orchid\Crud\Resource;
+use Orchid\Screen\Components\Cells\Boolean;
+use Orchid\Screen\Components\Cells\Currency;
+use Orchid\Screen\Components\Cells\DateTimeSplit;
+use Orchid\Screen\Fields\Cropper;
 use Orchid\Screen\Fields\Input;
+use Orchid\Screen\Fields\Picture;
+use Orchid\Screen\Fields\Relation;
 use Orchid\Screen\Sight;
 use Orchid\Screen\TD;
 
@@ -38,17 +46,63 @@ class ProductResource extends Resource
     public function columns(): array
     {
         return [
-            TD::make('id'),
-            TD::make('title'),
-            TD::make('created_at', 'Date of creation')
+            TD::make('id', 'ID')
+                ->width('120')
+                ->render(fn ($model) =>
+                "<img src='$model->imageUrl'
+                      alt='preview_image'
+                      class='mw-100 d-block img-fluid rounded-1 w-100'>
+                    <span class='small text-muted mt-1 mb-0'># {$model->id}</span>")
+                ->sort(),
+            TD::make('title')->sort(),
+            TD::make('slug')->defaultHidden(),
+            TD::make('description')->width(200)->defaultHidden(),
+            TD::make('color')
+                ->render(fn ($model) => "<div class='rounded' style='height:16px; width:16px; background:{$model->color}'></div>")
+                ->align(TD::ALIGN_CENTER),
+            TD::make('price')
+                ->usingComponent(Currency::class, after: '₽')
+                ->align(TD::ALIGN_CENTER)
+                ->sort(),
+            TD::make('tags', 'Tags')
+                ->render(
+                    function ($model) {
+                        return view('components.chip', [
+                            'tags' => $model->tags
+                        ]);
+                    }
+                )
+                ->align(TD::ALIGN_CENTER),
+            TD::make('sticker', 'Sticker')
+                ->defaultHidden()
+                ->render(
+                    function ($model) {
+                        return $model->sticker->title;
+                    }
+                )
+                ->align(TD::ALIGN_CENTER),
+            TD::make('category')
                 ->render(function ($model) {
-                    return $model->created_at->toDateTimeString();
-                }),
+                    return $model->category->title;
+                })
+                ->align(TD::ALIGN_CENTER),
+            TD::make('group_id', "Group")
+                ->render(function ($model) {
+                    return $model->group->title;
+                })
+                ->align(TD::ALIGN_CENTER),
+            TD::make('is_published', "Publised")->usingComponent(Boolean::class)
+                ->align(TD::ALIGN_CENTER)
+                ->sort(),
+            TD::make('created_at', 'Date of creation')
+                ->usingComponent(DateTimeSplit::class)
+                ->align(TD::ALIGN_RIGHT)
+                ->sort(),
 
             TD::make('updated_at', 'Update date')
-                ->render(function ($model) {
-                    return $model->updated_at->toDateTimeString();
-                }),
+                ->usingComponent(DateTimeSplit::class)
+                ->align(TD::ALIGN_RIGHT)
+                ->sort(),
         ];
     }
 
@@ -62,6 +116,14 @@ class ProductResource extends Resource
         return [
             Sight::make('id'),
             Sight::make('title'),
+
+            Sight::make('created_at', 'Date of creation')->render(function ($model) {
+                return $model->created_at->toDateTimeString();
+            }),
+
+            Sight::make('updated_at', 'Update date')->render(function ($model) {
+                return $model->updated_at->toDateTimeString();
+            }),
         ];
     }
 
@@ -72,7 +134,9 @@ class ProductResource extends Resource
      */
     public function filters(): array
     {
-        return [];
+        return [
+            new DefaultSorted('id', 'desc'),
+        ];
     }
 
     public static function perPage(): int
