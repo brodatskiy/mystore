03bcb041acbefe7269db0c32a00935c6bb407377
Brodatsky <brokoli133750@gmail.com>
2024-05-01 16:09:54 +0300

    new TD

--- app/Orchid/Layouts/Product/ProductListLayout.php
+++ app/Orchid/Layouts/Product/ProductListLayout.php
@@ -9,6 +9,8 @@ use Orchid\Screen\Actions\Button;
 use Orchid\Screen\Actions\DropDown;
 use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Actions\ModalToggle;
+use Orchid\Screen\Components\Cells\Boolean;
+use Orchid\Screen\Components\Cells\Currency;
 use Orchid\Screen\Components\Cells\DateTimeSplit;
 use Orchid\Screen\Fields\Input;
 use Orchid\Screen\Layouts\Card;
@@ -29,49 +31,64 @@ class ProductListLayout extends Table
     public function columns(): array
     {
         return [
-            TD::make('title', __('Title'))
-                ->sort()
-                ->cantHide()
-                ->filter(Input::make())
-                ->render(function ($product) {
-                    return Link::make($product->title)
-                        ->route('platform.systems.products.edit', $product->id);
-                }),
-
-            TD::make('price', __('Price'))
-                ->sort()
-                ->cantHide()
-                ->filter(Input::make()),
-
-            TD::make('created_at', __('Created'))
-                ->usingComponent(DateTimeSplit::class)
-                ->align(TD::ALIGN_RIGHT)
-                ->defaultHidden()
+            TD::make('id', 'ID')
+                ->width('120')
+                ->render(fn ($model) =>
+                "<img src='$model->imageUrl'
+                      alt='preview_image'
+                      class='mw-100 d-block img-fluid rounded-1 w-100'>
+                    <span class='small text-muted mt-1 mb-0'># {$model->id}</span>")
                 ->sort(),
-
-            TD::make('updated_at', __('Last edit'))
-                ->usingComponent(DateTimeSplit::class)
-                ->align(TD::ALIGN_RIGHT)
-                ->sort(),
-
-            TD::make(__('Actions'))
+            TD::make('title')->sort(),
+            TD::make('description')->width(200)->defaultHidden(),
+            TD::make('color')
+                ->render(fn ($model) => "<div class='rounded' style='height:16px; width:16px; background:{$model->color}'></div>")
+                ->align(TD::ALIGN_CENTER),
+            TD::make('price')
+                ->usingComponent(Currency::class, after: '₽')
                 ->align(TD::ALIGN_CENTER)
-                ->width('100px')
-                ->render(fn (Product $product) => DropDown::make()
-                    ->icon('bs.three-dots-vertical')
-                    ->list([
+                ->sort(),
+            TD::make('tags', 'Tags')
+                ->render(
+                    function ($model) {
+                        return view('components.chip', [
+                            'tags' => $model->tags
+                        ]);
+                    }
+                )
+                ->align(TD::ALIGN_CENTER),
+            TD::make('sticker', 'Sticker')
+                ->defaultHidden()
+                ->render(
+                    function ($model) {
+                        return $model->sticker->title;
+                    }
+                )
+                ->align(TD::ALIGN_CENTER),
+            TD::make('category')
+                ->render(function ($model) {
+                    return $model->category->title;
+                })
+                ->align(TD::ALIGN_CENTER),
+            TD::make('group_id', "Group")
+                ->render(function ($model) {
+                    return $model->group->title;
+                })
+                ->align(TD::ALIGN_CENTER),
 
-                        Link::make(__('Edit'))
-                            ->route('platform.systems.products.edit', $product->id)
-                            ->icon('bs.pencil'),
+            TD::make('is_published', "Publised")->usingComponent(Boolean::class)
+                ->align(TD::ALIGN_CENTER)
+                ->sort(),
 
-                        Button::make(__('Delete'))
-                            ->icon('bs.trash3')
-                            ->confirm(__('Once the account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.'))
-                            ->method('remove', [
-                                'id' => $product->id,
-                            ]),
-                    ])),
+            TD::make('created_at', 'Date of creation')
+                ->usingComponent(DateTimeSplit::class)
+                ->align(TD::ALIGN_RIGHT)
+                ->sort(),
+
+            TD::make('updated_at', 'Update date')
+                ->usingComponent(DateTimeSplit::class)
+                ->align(TD::ALIGN_RIGHT)
+                ->sort(),
         ];
     }
 }
