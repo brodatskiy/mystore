cd97950a69356d53318ed647dc764a910521ce46
Brodatsky <brokoli133750@gmail.com>
2024-05-05 16:46:22 +0300

    add admin panel link

--- resources/js/Layouts/ShopLayout.vue
+++ resources/js/Layouts/ShopLayout.vue
@@ -77,6 +77,12 @@ import ApplicationLogo from "@/Components/ApplicationLogo.vue";
                                         </template>
 
                                         <template #content>
+                                            <a
+                                                class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                                                :href="route('platform.index')"
+                                            >
+                                                {{ $t("Admin panel") }}
+                                            </a>
                                             <DropdownLink
                                                 :href="route('profile.edit')"
                                             >
