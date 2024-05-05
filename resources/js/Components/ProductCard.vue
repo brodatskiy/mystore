91eb5efb265d2ae750793f7173a7b60a5467bfaf
Brodatsky <brokoli133750@gmail.com>
2024-05-05 15:43:42 +0300

    edit

--- resources/js/Components/ProductCard.vue
+++ resources/js/Components/ProductCard.vue
@@ -4,7 +4,7 @@ defineProps(["product"]);
 <template>
   <Link :href="route('product.show', product)">
     <div class="flex items-center justify-center bg-gray-100">
-        <div class="mx-auto px-5">
+        <div class="mx-auto">
           <div class="max-w-xs cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
             <div class="relative">
             <img class="w-full rounded-lg object-cover object-center" :src="product.image" :alt="product.title" >
