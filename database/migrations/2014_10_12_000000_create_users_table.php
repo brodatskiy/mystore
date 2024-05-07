975697df412367f3a9450dbfd703381f2efded59
Brodatsky <brokoli133750@gmail.com>
2024-05-07 19:50:13 +0300

    join tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedSmallInteger('role')->default(0);
+            $table->jsonb('permissions')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
