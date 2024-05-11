053e200f147ac1eed078693149fa463b6f51583b
Brodatsky <brokoli133750@gmail.com>
2024-05-11 22:41:30 +0300

    edit

--- resources/js/Components/Filters/ProductPriceFilter.vue
+++ resources/js/Components/Filters/ProductPriceFilter.vue
@@ -1,36 +1,17 @@
 <script setup>
 import { ref, watch } from "vue";
 
-const emits = defineEmits(["setPrice"]);
-
 const props = defineProps({
-    min_price: String,
-    max_price: String,
+    price: Object,
 });
 
-const selectedMin = ref(props.min_price);
-const selectedMax = ref(props.max_price);
+const emits = defineEmits(["setPrice"]);
 
-const selectPrice = () => {
-    emits("setPrice", {
-        selectedMin: selectedMin.value,
-        selectedMax: selectedMax.value,
-    });
-};
+const price = ref(props.price);
 
-watch(
-    () => props.min_price,
-    () => {
-        selectedMin.value = props.min_price;
-    }
-);
-
-watch(
-    () => props.max_price,
-    () => {
-        selectedMax.value = props.max_price;
-    }
-);
+watch(price, (price) => {
+    emits("setPrice", price.value);
+});
 </script>
 
 <template>
@@ -43,7 +24,7 @@ watch(
                     type="number"
                     step="5"
                     min="0"
-                    v-model="selectedMin"
+                    v-model="price.min"
                     placeholder="Min"
                     class="w-full bg-zinc-200 text-zinc-600 placeholder-zinc-400 rounded border-none focus:ring focus:ring-zinc-300 focus:ring-opacity-80 text-xs"
                 />
@@ -51,17 +32,11 @@ watch(
                     type="number"
                     step="5"
                     min="0"
-                    v-model="selectedMax"
+                    v-model="price.max"
                     placeholder="Max"
                     class="w-full bg-zinc-200 text-zinc-600 placeholder-zinc-400 rounded border-none focus:ring focus:ring-zinc-300 focus:ring-opacity-80 text-xs"
                 />
             </div>
-            <button
-                @click="selectPrice()"
-                class="text-xs bg-c-green-600 hover:bg-c-green-300 max-w-max py-2 px-4 text-white mt-3 rounded"
-            >
-                Apply
-            </button>
         </div>
     </section>
 </template>
