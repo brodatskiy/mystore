5cb080ee1ad2ac2cfd81dc3734d3deb3d1c3b488
Brodatsky <brokoli133750@gmail.com>
2024-05-13 13:46:29 +0300

    edit

--- resources/js/Components/ProductCard.vue
+++ resources/js/Components/ProductCard.vue
@@ -6,7 +6,7 @@ defineProps(["product"]);
         <Link :href="route('product.show', product)">
             <div class="max-w-72 cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
                 <div class="relative">
-                    <div v-if="!product.imagew" class="w-full h-72 bg-gray-300 rounded" ></div>
+                    <div v-if="!product.image" class="w-full h-72 bg-gray-300 rounded" ></div>
                     <img v-else class="w-full h-72 rounded-lg object-cover object-center"  :src="product.image" :alt="product.title" >
                     <div class="absolute top-2 right-2">
                         {{ product.sticker.title.toUpperCase() }}
