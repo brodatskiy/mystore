fc27cfe06d7a0d13834f4b5c1ae5cebf9c6d15de
Brodatsky <brokoli133750@gmail.com>
2024-05-13 20:39:44 +0300

    add sidebar

--- resources/js/Pages/Client/Home/Index.vue
+++ resources/js/Pages/Client/Home/Index.vue
@@ -5,12 +5,10 @@ import ShopLayout from "@/Layouts/ShopLayout.vue";
 
 import ProductCard from "@/Components/ProductCard.vue";
 import Pagination from "@/Components/Pagination.vue";
-import ProductCategoryFilter from "@/Components/Filters/ProductCategoryFilter.vue";
 import ProductPriceFilter from "@/Components/Filters/ProductPriceFilter.vue";
 import ProductSearch from "@/Components/Filters/ProductSearch.vue";
 // import AppliedFilters from "@/Components/Filters/AppliedFilters.vue";
 import SortProducts from "@/Components/Filters/SortProducts.vue";
-import debounce from "lodash/debounce";
 import pickBy from "lodash/pickBy";
 
 import { usePage } from "@inertiajs/vue3";
@@ -51,55 +49,72 @@ watch(filters.value, (value) => {
         replace: true,
     });
 });
+
+const visible = ref(false);
 </script>
 <template>
     <Head title="Home" />
 
     <ShopLayout>
-        <div class="pb-2 flex">
-            <ul class="flex justify-center mx-auto max-w-2xl">
-                <li
-                    v-for="category in categories"
-                    :key="category.id"
-                    class="text-zinc-500 cursor-pointer px-2 py-1"
-                >
-                    <Link :href="route('catalog', category.slug)">{{
-                        category.title
-                    }}</Link>
-                </li>
-            </ul>
-
-            <SortProducts :sort="props.sort" @setSort="setSort"></SortProducts>
+        <div class="relative flex">
+            <div class="mx-auto">
+                <ul class="flex">
+                    <li
+                        v-for="category in categories"
+                        :key="category.id"
+                        class="text-zinc-500 cursor-pointer px-2 py-1"
+                    >
+                        <Link :href="route('catalog', category.slug)">{{
+                            category.title
+                        }}</Link>
+                    </li>
+                </ul>
+            </div>
+            <div class="absolute right-0 flex space-x-8">
+                <button @click="visible = true">Filters</button>
+                <SortProducts
+                    :sort="props.sort"
+                    @setSort="setSort"
+                ></SortProducts>
+            </div>
         </div>
-        <div class="flex">
-            <div class="space-y-2 p-4">
-                <ProductSearch
-                    :search="props.search"
-                    @setSearch="setSearch"
-                ></ProductSearch>
 
-                <ProductPriceFilter
-                    :price="props.price"
-                    @setPrice="setPrice"
-                ></ProductPriceFilter>
+        <div class="flex flex-col">
+            <div
+                class="mx-auto grid max-w-6xl grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4"
+            >
+                <ProductCard
+                    v-for="product in products.data"
+                    :key="product.id"
+                    :product="product"
+                ></ProductCard>
             </div>
-            <div class="flex-1">
-                <div
-                    class="mx-auto grid max-w-6xl grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4"
-                >
-                    <ProductCard
-                        v-for="product in products.data"
-                        :key="product.id"
-                        :product="product"
-                    ></ProductCard>
+
+            <Pagination
+                v-if="products.links.next || products.links.prev"
+                :links="products.meta.links"
+                :meta="products.links"
+            ></Pagination>
+        </div>
+
+        <div class="card flex justify-content-center">
+            <Sidebar
+                v-model:visible="visible"
+                header="Filters"
+                position="right"
+            >
+                <div class="space-y-2 p-4">
+                    <ProductSearch
+                        :search="props.search"
+                        @setSearch="setSearch"
+                    ></ProductSearch>
+
+                    <ProductPriceFilter
+                        :price="props.price"
+                        @setPrice="setPrice"
+                    ></ProductPriceFilter>
                 </div>
-
-                <Pagination
-                    v-if="products.links.next || products.links.prev"
-                    :links="products.meta.links"
-                    :meta="products.links"
-                ></Pagination>
-            </div>
+            </Sidebar>
         </div>
     </ShopLayout>
 </template>
