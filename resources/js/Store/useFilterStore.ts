6536efc97547e6b8755aad31efd81c8f736987d9
Brodatsky <brokoli133750@gmail.com>
2024-05-15 23:23:28 +0300

    init

---
+++ resources/js/Store/useFilterStore.ts
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+import { router } from "@inertiajs/vue3";
+import pickBy from "lodash/pickBy";
+
+export const useFilterStore = defineStore("Filter", () => {
+    const price = ref([]);
+    const sort = ref();
+    const search = ref();
+
+    const filters = computed(() => {
+        return { price: price.value, sort: sort.value, search: search.value };
+    });
+
+    function setStore(props) {
+        price.value = props.price;
+        sort.value = props.sort;
+        search.value = props.search;
+    }
+
+    function applyFilters(currentLocation) {
+        router.get(currentLocation, pickBy(filters.value), {
+            preserveState: true,
+            replace: true,
+        });
+    }
+
+    return { sort, price, search, filters, applyFilters, setStore };
+});
