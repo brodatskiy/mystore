01dc6d543747d00f40f287a27f45869939650fbb
Brodatsky <brokoli133750@gmail.com>
2024-05-17 11:55:19 +0300

    update

--- resources/js/Pages/Client/Catalog/Index.vue
+++ resources/js/Pages/Client/Catalog/Index.vue
@@ -1,40 +1,103 @@
 <script setup>
-import { ref, onMounted, computed } from "vue";
+import { ref } from "vue";
+import { usePage } from "@inertiajs/vue3";
 
 import ShopLayout from "@/Layouts/ShopLayout.vue";
-import { Head, Link } from "@inertiajs/vue3";
-
 import ProductCard from "@/Components/ProductCard.vue";
+import Pagination from "@/Components/Pagination.vue";
+import ProductPriceFilter from "@/Components/Filters/ProductPriceFilter.vue";
+import ProductSearch from "@/Components/Filters/ProductSearch.vue";
+import SortProducts from "@/Components/Filters/SortProducts.vue";
 
-const props = defineProps(["products"]);
+import { useFilterStore } from "@/Store/useFilterStore";
 
-const cols = computed(() => {
-    switch (name.value) {
-        case "md":
-            return 4;
-        case "lg":
-            return 3;
-        default:
-            return 6;
-    }
+const filterStore = useFilterStore();
 
-    return undefined;
+const currentLocation = usePage().props.ziggy.location;
+
+const props = defineProps({
+    products: Object,
+    categories: Array,
+    tags: Array,
+    search: String,
+    price: Array,
+    sort: String,
 });
+
+filterStore.setStore(props);
+
+function applyFilters() {
+    filterStore.applyFilters(currentLocation, filters);
+}
+
+const filterExpand = ref(false);
 </script>
 <template>
-    <Head title="Catalog" />
+    <Head title="Home" />
 
     <ShopLayout>
-        <v-container class="pa-0">
-            <v-row>
-                <v-col
-                    v-for="product in products"
-                    :key="product.id"
-                    :cols="cols"
+        <div class="flex justify-end">
+            <div class="flex space-x-8">
+                <button
+                    @click="filterExpand = true"
+                    class="flex items-center hover:text-primary-600"
                 >
-                    <ProductCard :product="product"></ProductCard>
-                </v-col>
-            </v-row>
-        </v-container>
+                    <Icon
+                        icon="mdi:filter-variant"
+                        width="1.5rem"
+                        height="1.5rem"
+                    />
+                    <span>Filters</span>
+                </button>
+                <SortProducts
+                    :sort="props.sort"
+                    @change="applyFilters"
+                ></SortProducts>
+            </div>
+        </div>
+
+        <div class="flex flex-col">
+            <div
+                class="mx-auto grid max-w-6xl grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4"
+            >
+                <ProductCard
+                    v-for="product in products.data"
+                    :key="product.id"
+                    :product="product"
+                ></ProductCard>
+            </div>
+
+            <Pagination
+                v-if="products.links.next || products.links.prev"
+                :links="products.meta.links"
+                :meta="products.links"
+            ></Pagination>
+        </div>
+
+        <div>
+            <Sidebar
+                v-model:visible="filterExpand"
+                header="Filters"
+                position="right"
+                :modal="false"
+            >
+                <div class="space-y-8 p-4">
+                    <div>
+                        <ProductSearch></ProductSearch>
+                    </div>
+
+                    <div>
+                        <ProductPriceFilter></ProductPriceFilter>
+                    </div>
+
+                    <div class="flex justify-center">
+                        <Button @click="applyFilters" class="w-full">
+                            Apply</Button
+                        >
+                    </div>
+                </div>
+            </Sidebar>
+        </div>
     </ShopLayout>
 </template>
+applyFilters
