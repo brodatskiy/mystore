7a165e331eecfa51c98dd2e28fbc4f3e95b7e036
Brodatsky <brokoli133750@gmail.com>
2024-05-26 14:32:05 +0300

    init

--- resources/js/Store/useCartStore.ts
+++ resources/js/Store/useCartStore.ts
@@ -1,62 +1,16 @@
 import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+import { router } from "@inertiajs/vue3";
 import { CART_STORE } from "@/Utils/usePersistCart";
 
-import type { Product } from "@/Store/useProductStore";
+export const useCartStore = defineStore("Cart", () => {
+    const products = ref(JSON.parse(localStorage.getItem(CART_STORE)) ?? []);
 
-export interface CartProduct {
-    product: Product;
-    quantity: number;
-}
+    // const total = computed(() => {
+    //     return products.value.reduce((acc, product) => {
+    //         return acc + product.price * product.quantity;
+    //     }, 0);
+    // });
 
-interface CartState {
-    contents: Record<string, CartProduct>;
-}
-
-export const useCartStore = defineStore("CartStore", {
-    state: (): CartState => {
-        return {
-            contents:
-                JSON.parse(localStorage.getItem(CART_STORE) as string) ?? {},
-        };
-    },
-
-    getters: {
-        count(): number {
-            return Object.keys(this.contents).reduce((acc, id) => {
-                return acc + this.contents[id].quantity;
-            }, 0);
-        },
-
-        total(): number {
-            return Object.keys(this.contents).reduce((acc, id) => {
-                return (
-                    acc +
-                    this.contents[id].product.price * this.contents[id].quantity
-                );
-            }, 0);
-        },
-    },
-    actions: {
-        add(product: Product) {
-            if (this.contents[product.id]) {
-                this.contents[product.id].quantity += 1;
-            } else {
-                this.contents[product.id] = {
-                    product,
-                    quantity: 1,
-                };
-            }
-        },
-        remove(product: Product) {
-            if (!this.contents[product.id]) {
-                return;
-            }
-
-            this.contents[product.id].quantity -= 1;
-
-            if (this.contents[product.id].quantity === 0) {
-                delete this.contents[product.id];
-            }
-        },
-    },
+    return { products };
 });
