13adcb115d96a76fd746500c727df521ff5f1cac
Brodatsky <brokoli133750@gmail.com>
2024-05-29 16:13:34 +0300

    add wishs , orders routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Client\WishController;
+use App\Http\Controllers\Client\WishlistController;
 use App\Http\Controllers\Client\CartController;
 use App\Http\Controllers\Client\CatalogController;
 use App\Http\Controllers\Client\HomeController;
@@ -8,6 +8,7 @@ use App\Http\Controllers\Client\MessageController;
 use App\Http\Controllers\Client\OrderController;
 use App\Http\Controllers\Client\ProductController;
 use App\Http\Controllers\Client\ProfileController;
+use App\Http\Controllers\Client\WishController;
 use App\Http\Controllers\SetLocaleController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,16 +27,20 @@ Route::post('/locale', [SetLocaleController::class, 'locale'])->name('locale');
 
 //Shop
 Route::get('/',  HomeController::class)->name('/');
-Route::get('/wish', [WishController::class, 'index'])->name('wish');
-Route::get('/order', [OrderController::class, 'index'])->name('order');
+Route::get('/wishlist', [WishController::class, 'index'])->name('wishlist');
 Route::get('/catalog/{category:slug}', CatalogController::class)->name('catalog');
 Route::resource('/product', ProductController::class)->only('show');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/orderlist', [OrderController::class, 'index'])->name('orderlist');
+});
 
 //Cart
 Route::controller(CartController::class)
     ->prefix('cart')
     ->group(function () {
         Route::get('/', 'index')->name('cart.index');
+        Route::post('/order', 'order')->name('cart.order');
         Route::post('/{product}/add', 'add')->name('cart.add');
         Route::patch('/{product}/increase', 'increase')->name('cart.increase');
         Route::patch('/{product}/decrease', 'decrease')->name('cart.decrease');
