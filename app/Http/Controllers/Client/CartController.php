a8ae444ef1c209b5b8f36c1669ba43dda6309621
Brodatsky <brokoli133750@gmail.com>
2024-05-29 16:09:55 +0300

    add order()

--- app/Http/Controllers/Client/CartController.php
+++ app/Http/Controllers/Client/CartController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\Client;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CartItem\CartItemResource;
 use App\Models\Cart;
+use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class CartController extends Controller
@@ -14,11 +17,47 @@ class CartController extends Controller
     public function index()
     {
         $cartItems = Cart::getItems();
-
         return Inertia::render('Client/Cart/Index', [
             'products' => CartItemResource::collection($cartItems),
             'total' => Cart::getTotal()
         ]);
+    }
+
+    public function order()
+    {
+        if (auth()->check()) {
+            try {
+                DB::beginTransaction();
+
+                $order = Order::create([
+                    'user_id' => auth()->id(),
+                    'total' => Cart::getTotal(),
+                    'status' => 'Unpaid'
+                ]);
+
+                $cartItems = Cart::getItems();
+
+                foreach ($cartItems as $cartItem) {
+                    $order->orderItems()->create([
+                        'order_id' => $order->id,
+                        'product_id' => $cartItem->product_id,
+                        'price' => $cartItem->price,
+                        'quantity' => $cartItem->quantity,
+                    ]);
+                }
+
+                Cart::get()->delete();
+
+                Db::commit();
+
+                return back();
+            } catch (\Exception $exeption) {
+                Db::rollBack();
+                abort(500);
+            }
+        } else {
+            return back();
+        }
     }
 
     public function add(Product $product): RedirectResponse
