12d4ce5374b58629c9524558eebb682f975af4d2
Brodatsky <brokoli133750@gmail.com>
2024-05-30 22:28:46 +0300

    add index() , add()

--- app/Http/Controllers/Client/WishController.php
+++ app/Http/Controllers/Client/WishController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
-use App\Models\Wishlist;
+use App\Http\Resources\Wish\WishResource;
+use App\Models\Product;
+use App\Models\Wish;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -12,10 +15,27 @@ class WishController extends Controller
     public function index()
     {
         $user = auth()->user();
-        $wishlist = $user->wishes()->get() ?? collect([]);
+        $wishlist = $user?->wishes()->get() ?? collect([]);
+        $wishlist = Wish::where('user_id', auth()->id())->with('product')->get();
 
+        $wishlist = WishResource::collection($wishlist);
+        // dd($wishlist);
         return Inertia::render('Client/Wishlist/Index', [
             'wishlist' => $wishlist,
         ]);
+    }
+
+    public function add(Product $product): RedirectResponse
+    {
+
+        $user = auth()->user();
+        $user->wishes()->toggle([$product->id]);
+        // Wish::firstOrCreate([
+        //     'user_id' => auth()->id()
+        // ], [
+        //     'product_id' => $product->id,
+        // ]);
+
+        return back();
     }
 }
