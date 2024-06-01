b297b85aae4c1e1c532e274b6caa2d6d4d0f5794
Brodatsky <brokoli133750@gmail.com>
2024-06-01 15:26:12 +0300

    Pivot

--- app/Models/Wish.php
+++ app/Models/Wish.php
@@ -2,12 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class Wish extends Model
+class Wish extends Pivot
 {
     use HasFactory;
 
