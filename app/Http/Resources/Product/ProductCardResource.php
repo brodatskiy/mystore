81768ded1cd68b1b3ed77260c6d643ceaed3ef2b
Brodatsky <brokoli133750@gmail.com>
2024-06-04 16:47:15 +0300

    wished

--- app/Http/Resources/Product/ProductCardResource.php
+++ app/Http/Resources/Product/ProductCardResource.php
@@ -10,10 +10,18 @@ class ProductCardResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+
+        if (auth()->check()) {
+            $wished = $this->wishedBy()->wherePivot('user_id', auth()->user()->id)->first() ? true : false;
+        } else {
+            $wished = false;
+        }
+
         return [
             'id' => $this->id,
             'title' => $this->title,
             'image' => $this->preview_image,
+            'wished' => $wished,
             'price' => $this->price,
             'sticker' => new StickerResource($this->sticker),
         ];
