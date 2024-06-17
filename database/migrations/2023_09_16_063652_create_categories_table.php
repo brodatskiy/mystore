84f4075ba29df0f9f5dac18e8d1f374deebbd660
Brodatsky <brokoli133750@gmail.com>
2024-06-17 16:17:46 +0300

    add parent_id column

--- database/migrations/2023_09_16_063652_create_categories_table.php
+++ database/migrations/2023_09_16_063652_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->foreignId('parent_id')->index()->nullable()->constrained('categories');
             $table->string('slug')->unique();
             $table->timestamps();
 
