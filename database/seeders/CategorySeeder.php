43b4d1d9d524c0912d3a0d2b2bb25dd99deb10d0
Brodatsky <brokoli133750@gmail.com>
2024-06-18 22:02:43 +0300

    new categories

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,11 +14,10 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        Category::factory()->create(['title' => 'Sneakers']);
-        Category::factory()->create(['title' => 'Boots']);
-        Category::factory()->create(['title' => 'Shorts']);
-        Category::factory()->create(['title' => 'Jeans']);
-        Category::factory()->create(['title' => 'T-shirt']);
+        Category::factory()->create(['title' => 'Clothing']);
+        Category::factory()->create(['title' => 'Shoes']);
+        Category::factory()->create(['title' => 'Jewelry']);
+        Category::factory()->create(['title' => 'Handbags']);
         Category::factory()->create(['title' => 'Hoodie']);
         Category::factory()->create(['title' => 'Sweatshirt']);
         Category::factory()->create(['title' => 'Hats']);
