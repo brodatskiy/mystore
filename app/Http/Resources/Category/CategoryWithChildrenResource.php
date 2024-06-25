bac5675d1b2a7db3e49eae335b8fe6a37ecb0e46
Brodatsky <brokoli133750@gmail.com>
2024-06-25 12:56:25 +0300

    init

---
+++ app/Http/Resources/Category/CategoryWithChildrenResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Category;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryWithChildrenResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'section_id' => $this->section_id,
+            'parent_id' => $this->parent_id,
+            'children' => CategoryResource::collection($this->children),
+        ];
+    }
+}
---
+++ app/Http/Resources/Section/SectionWithCategoriesResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Section;
+
+use App\Http\Resources\Category\CategoryWithChildrenResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SectionWithCategoriesResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'parentCategories' => CategoryWithChildrenResource::collection($this->parentCategories->load('children')),
+        ];
+    }
+}
