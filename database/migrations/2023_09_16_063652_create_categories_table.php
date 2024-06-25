49169e275671f165ebdc09310b3cae8499b1ad3f
Brodatsky <brokoli133750@gmail.com>
2024-06-25 12:52:51 +0300

    add fkey for sections

--- database/migrations/2023_09_16_063652_create_categories_table.php
+++ database/migrations/2023_09_16_063652_create_categories_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,11 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->foreignId('section_id')
+                ->index()
+                ->constrained('sections')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();;
             $table->foreignId('parent_id')->index()->nullable()->constrained('categories');
             $table->string('slug')->unique();
             $table->timestamps();
