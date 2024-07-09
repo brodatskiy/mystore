c7cad0738ef4796f311c1baf0e3a03ba072cfa29
Brodatskiy <nikolaybrodatskiy@gmail.com>
2024-07-09 19:05:53 +0300

    add orchid traits

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Orchid\Attachment\Attachable;
+use Orchid\Filters\Filterable;
+use Orchid\Screen\AsSource;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
@@ -12,6 +15,7 @@ class Section extends Model
 {
     use HasFactory;
     use HasSlug;
+    use AsSource, Filterable, Attachable;
 
     protected $guarded = false;
 
