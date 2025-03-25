2fdccddd910aeb38c809040f702961cdb2b30982
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-03-25 21:14:20 +0300

    реализован darkmode

--- resources/js/Components/ProductCard.vue
+++ resources/js/Components/ProductCard.vue
@@ -1,7 +1,6 @@
 <script setup>
 import {router} from "@inertiajs/vue3";
 import {Link} from "@inertiajs/vue3";
-import 'primeicons/primeicons.css'
 
 defineProps(["product"]);
 
@@ -14,35 +13,33 @@ function toggleWish(product) {
 }
 </script>
 <template>
-    <div class="">
-        <Link :href="route('product.show', product)">
-            <div
-                class="max-w-72 cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
-                <div class="relative">
-                    <div v-if="!product.image" class="w-full h-72 bg-gray-300 rounded"></div>
-                    <img v-else class="w-full h-72 rounded-lg object-cover object-center" :src="product.image"
-                         :alt="product.title">
-                        <p v-if="product.sticker" class="absolute top-2 right-2">
-                            {{ product.sticker.title.toUpperCase() }}
-                        </p>
-                    </img>
-                </div>
-                <div class="flex items-center justify-between py-2">
-                    <p class=" text-l font-semibold text-gray-800">${{ product.price }}</p>
-                    <p class="font-bold text-gray-500">{{ product.title }}</p>
-                    <Button v-if="$page.props.auth.user" @click.prevent="toggleWish(product)">
-                        <i v-if="product.wished" class="pi pi-heart-fill"></i>
-                        <i v-else class="pi pi-heart"></i>
-                    </Button>
-                </div>
-                <Button
-                    class="w-full"
-                    @click.prevent="add(product)"
-                >
-                    Add to cart
+    <Link :href="route('product.show', product)">
+        <div
+            class="max-w-72 cursor-pointer rounded-lg bg-surface-100 dark:bg-surface-800 p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
+            <div class="relative">
+                <div v-if="!product.image" class="w-full h-72 bg-gray-300 rounded"></div>
+                <img v-else class="w-full h-72 rounded-lg object-cover object-center" :src="product.image"
+                     :alt="product.title">
+                <p v-if="product.sticker" class="absolute top-2 right-2">
+                    {{ product.sticker.title.toUpperCase() }}
+                </p>
+                </img>
+            </div>
+            <div class="flex items-center justify-between py-2">
+                <p class=" text-l font-semibold text-surface-800 dark:text-surface-200">${{ product.price }}</p>
+                <p class="font-bold text-surface-800 dark:text-surface-200">{{ product.title }}</p>
+                <Button severity="secondary" v-if="$page.props.auth.user" @click.prevent="toggleWish(product)">
+                    <i v-if="product.wished" class="pi pi-heart-fill" style="color: red"></i>
+                    <i v-else class="pi pi-heart" style="color: red"></i>
                 </Button>
             </div>
-        </Link>
-    </div>
+            <Button
+                class="w-full"
+                @click.prevent="add(product)"
+            >
+                Add to cart
+            </Button>
+        </div>
+    </Link>
 </template>
 <style scoped></style>
