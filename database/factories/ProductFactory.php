8718f9e720b3cb56948f1fdb48d6a00da088cc26
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-04-03 10:57:58 +0300

    изменена генерация картинок, добавлен рейтинг

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,9 +6,6 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Models\Sticker;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Http\File;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
 
 /**
  * @extends Factory<Product>
@@ -20,16 +17,31 @@ class ProductFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
+        $randomImages =[
+            'https://m.media-amazon.com/images/I/41WpqIvJWRL._AC_UY436_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/61ghDjhS8vL._AC_UY436_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/61c1QC4lF-L._AC_UY436_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/710VzyXGVsL._AC_UY436_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/61EPT-oMLrL._AC_UY436_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/71r3ktfakgL._AC_UY436_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/61CqYq+xwNL._AC_UL640_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/71cVOgvystL._AC_UL640_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/71E+oh38ZqL._AC_UL640_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/61uSHBgUGhL._AC_UL640_QL65_.jpg',
+            'https://m.media-amazon.com/images/I/71nDK2Q8HAL._AC_UL640_QL65_.jpg'
+        ];
+
         return [
             'title' => fake()->words(1, true),
             'description' => fake()->paragraph(1),
-            'preview_image' => fake()->imageUrl(300, 400),
+            'preview_image' => $randomImages[rand(0, 10)],
             'color' => fake()->safeHexColor(),
             'category_id' => Category::all()->random()->id,
             'sticker_id' => Sticker::all()->random()->id,
             'price' => fake()->numberBetween(100, 1000),
+            'rating' => fake()->randomFloat(1,0, 10),
             'is_published' => 1
         ];
     }
