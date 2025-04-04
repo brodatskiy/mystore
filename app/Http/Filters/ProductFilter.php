44d4df23c7e40a667536323866b89deaf761d5e0
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-04-04 12:12:33 +0300

    Добавлены типы

--- app/Http/Filters/ProductFilter.php
+++ app/Http/Filters/ProductFilter.php
@@ -8,11 +8,9 @@ use Illuminate\Database\Eloquent\Builder;
 
 class ProductFilter extends AbstractFilter
 {
-    public const SEARCH = 'search';
-    public const CATEGORY_ID = 'category_id';
-    public const PRICE = 'price';
-
-
+    public const string SEARCH = 'search';
+    public const string CATEGORY_ID = 'category_id';
+    public const string PRICE = 'price';
 
     protected function getCallbacks(): array
     {
@@ -23,18 +21,18 @@ class ProductFilter extends AbstractFilter
         ];
     }
 
-    public function search(Builder $builder, $value)
+    public function search(Builder $builder, $value): void
     {
         $builder->where('products.' . 'title', 'like', "%{$value}%");
     }
 
-    public function categoryId(Builder $builder, $value)
+    public function categoryId(Builder $builder, $value): void
     {
 
         $builder->where('category_id', $value);
     }
 
-    public function price(Builder $builder, $value)
+    public function price(Builder $builder, $value): void
     {
         $builder->whereBetween('price', [$value[0], $value[1]]);
     }
