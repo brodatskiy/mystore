3dd24be917a561a25efd742e83bb0c3640bff178
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-04-21 14:37:45 +0300

    добавлен класс cursor-pointer

--- resources/js/Components/Buttons/ButtonPrimary.vue
+++ resources/js/Components/Buttons/ButtonPrimary.vue
@@ -1,7 +1,7 @@
 <template>
     <button
         v-bind="$attrs"
-        class="px-3 py-2 rounded-md bg-primary-500 hover:bg-primary-400 text-white transition-all "
+        class="px-3 py-2 rounded-md bg-primary-500 hover:bg-primary-400 cursor-pointer text-white transition-all "
     >
         <slot/>
     </button>
