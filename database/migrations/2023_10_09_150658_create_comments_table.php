71e16fd4771cb59ca0437186dea0a9602b3b05f9
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-04-21 16:03:34 +0300

    изменено имя колонки

--- database/migrations/2023_10_09_150658_create_comments_table.php
+++ database/migrations/2023_10_09_150658_create_comments_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->text('messege');
+            $table->text('body');
             $table->timestamps();
 
             $table->foreignIdFor(User::class)
