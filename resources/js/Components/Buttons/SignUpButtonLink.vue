fbf455af60d90bcf9f40f23aeef7d7452d72dcc4
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-05-13 13:05:25 +0300

    Убрал лишнюю вложенность тега <button>

--- resources/js/Components/Buttons/SignUpButtonLink.vue
+++ resources/js/Components/Buttons/SignUpButtonLink.vue
@@ -3,11 +3,10 @@
 import {Link} from "@inertiajs/vue3";
 </script>
 <template>
-    <button class="rounded-md bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 hover:from-pink-500 hover:to-indigo-500 text-white border-none px-4 py-2 font-bold cursor-pointer transition-all">
-        <Link
-            v-bind="$attrs"
-        >
-            SIGN UP
-        </Link>
-    </button>
+    <Link
+        v-bind="$attrs"
+        class="rounded-md bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 hover:from-pink-500 hover:to-indigo-500 text-white border-none px-4 py-2 font-bold cursor-pointer transition-all"
+    >
+        SIGN UP
+    </Link>
 </template>
