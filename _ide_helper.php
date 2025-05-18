be7094b9c56023cf09780fef7658ed94db757176
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-05-18 16:12:53 +0300

    Обновляет информацию о версии Laravel в файле _ide_helper.php.

--- _ide_helper.php
+++ _ide_helper.php
@@ -5,7 +5,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 11.44.2.
+ * Generated for Laravel 11.44.7.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
@@ -3319,6 +3319,20 @@ namespace Illuminate\Support\Facades {
         {
             /** @var \Illuminate\View\Compilers\BladeCompiler $instance */
             $instance->precompiler($precompiler);
+        }
+
+        /**
+         * Execute the given callback using a custom echo format.
+         *
+         * @param string $format
+         * @param callable $callback
+         * @return string 
+         * @static 
+         */
+        public static function usingEchoFormat($format, $callback)
+        {
+            /** @var \Illuminate\View\Compilers\BladeCompiler $instance */
+            return $instance->usingEchoFormat($format, $callback);
         }
 
         /**
@@ -12858,6 +12872,217 @@ namespace Illuminate\Support\Facades {
         }
 
         /**
+         * 
+         *
+         * @throws AMQPProtocolChannelException
+         * @static 
+         */
+        public static function laterRaw($delay, $payload, $queue = null, $attempts = 0)
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->laterRaw($delay, $payload, $queue, $attempts);
+        }
+
+        /**
+         * 
+         *
+         * @throws AMQPProtocolChannelException
+         * @static 
+         */
+        public static function bulkRaw($payload, $queue = null, $options = [])
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->bulkRaw($payload, $queue, $options);
+        }
+
+        /**
+         * 
+         *
+         * @throws RuntimeException
+         * @static 
+         */
+        public static function getConnection()
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->getConnection();
+        }
+
+        /**
+         * 
+         *
+         * @static 
+         */
+        public static function setConnection($connection)
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->setConnection($connection);
+        }
+
+        /**
+         * Job class to use.
+         *
+         * @throws Throwable
+         * @static 
+         */
+        public static function getJobClass()
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->getJobClass();
+        }
+
+        /**
+         * Gets a queue/destination, by default the queue option set on the connection.
+         *
+         * @static 
+         */
+        public static function getQueue($queue = null)
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->getQueue($queue);
+        }
+
+        /**
+         * Checks if the given exchange already present/defined in RabbitMQ.
+         * 
+         * Returns false when the exchange is missing.
+         *
+         * @throws AMQPProtocolChannelException
+         * @static 
+         */
+        public static function isExchangeExists($exchange)
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->isExchangeExists($exchange);
+        }
+
+        /**
+         * Declare an exchange in rabbitMQ, when not already declared.
+         *
+         * @static 
+         */
+        public static function declareExchange($name, $type = 'direct', $durable = true, $autoDelete = false, $arguments = [])
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->declareExchange($name, $type, $durable, $autoDelete, $arguments);
+        }
+
+        /**
+         * Delete an exchange from rabbitMQ, only when present in RabbitMQ.
+         *
+         * @throws AMQPProtocolChannelException
+         * @static 
+         */
+        public static function deleteExchange($name, $unused = false)
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->deleteExchange($name, $unused);
+        }
+
+        /**
+         * Checks if the given queue already present/defined in RabbitMQ.
+         * 
+         * Returns false when the queue is missing.
+         *
+         * @throws AMQPProtocolChannelException
+         * @static 
+         */
+        public static function isQueueExists($name = null)
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->isQueueExists($name);
+        }
+
+        /**
+         * Declare a queue in rabbitMQ, when not already declared.
+         *
+         * @static 
+         */
+        public static function declareQueue($name, $durable = true, $autoDelete = false, $arguments = [])
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->declareQueue($name, $durable, $autoDelete, $arguments);
+        }
+
+        /**
+         * Delete a queue from rabbitMQ, only when present in RabbitMQ.
+         *
+         * @throws AMQPProtocolChannelException
+         * @static 
+         */
+        public static function deleteQueue($name, $if_unused = false, $if_empty = false)
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->deleteQueue($name, $if_unused, $if_empty);
+        }
+
+        /**
+         * Bind a queue to an exchange.
+         *
+         * @static 
+         */
+        public static function bindQueue($queue, $exchange, $routingKey = '')
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->bindQueue($queue, $exchange, $routingKey);
+        }
+
+        /**
+         * Purge the queue of messages.
+         *
+         * @static 
+         */
+        public static function purge($queue = null)
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->purge($queue);
+        }
+
+        /**
+         * Acknowledge the message.
+         *
+         * @static 
+         */
+        public static function ack($job)
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->ack($job);
+        }
+
+        /**
+         * Reject the message.
+         *
+         * @static 
+         */
+        public static function reject($job, $requeue = false)
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->reject($job, $requeue);
+        }
+
+        /**
+         * Close the connection to RabbitMQ.
+         *
+         * @throws Exception
+         * @static 
+         */
+        public static function close()
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->close();
+        }
+
+        /**
+         * 
+         *
+         * @static 
+         */
+        public static function getChannel($forceNew = false)
+        {
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
+            return $instance->getChannel($forceNew);
+        }
+
+        /**
          * Get the maximum number of attempts for an object-based queue handler.
          *
          * @param mixed $job
@@ -12867,7 +13092,7 @@ namespace Illuminate\Support\Facades {
         public static function getJobTries($job)
         {
             //Method inherited from \Illuminate\Queue\Queue 
-            /** @var \Illuminate\Queue\SyncQueue $instance */
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
             return $instance->getJobTries($job);
         }
 
@@ -12881,7 +13106,7 @@ namespace Illuminate\Support\Facades {
         public static function getJobBackoff($job)
         {
             //Method inherited from \Illuminate\Queue\Queue 
-            /** @var \Illuminate\Queue\SyncQueue $instance */
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
             return $instance->getJobBackoff($job);
         }
 
@@ -12895,7 +13120,7 @@ namespace Illuminate\Support\Facades {
         public static function getJobExpiration($job)
         {
             //Method inherited from \Illuminate\Queue\Queue 
-            /** @var \Illuminate\Queue\SyncQueue $instance */
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
             return $instance->getJobExpiration($job);
         }
 
@@ -12909,7 +13134,7 @@ namespace Illuminate\Support\Facades {
         public static function createPayloadUsing($callback)
         {
             //Method inherited from \Illuminate\Queue\Queue 
-            \Illuminate\Queue\SyncQueue::createPayloadUsing($callback);
+            \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue::createPayloadUsing($callback);
         }
 
         /**
@@ -12921,7 +13146,7 @@ namespace Illuminate\Support\Facades {
         public static function getContainer()
         {
             //Method inherited from \Illuminate\Queue\Queue 
-            /** @var \Illuminate\Queue\SyncQueue $instance */
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
             return $instance->getContainer();
         }
 
@@ -12935,7 +13160,7 @@ namespace Illuminate\Support\Facades {
         public static function setContainer($container)
         {
             //Method inherited from \Illuminate\Queue\Queue 
-            /** @var \Illuminate\Queue\SyncQueue $instance */
+            /** @var \VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue $instance */
             $instance->setContainer($container);
         }
 
@@ -24504,6 +24729,29 @@ namespace Illuminate\Console\Scheduling {
         public static function sentryMonitor($monitorSlug = null, $checkInMargin = null, $maxRuntime = null, $updateMonitorConfig = true, $failureIssueThreshold = null, $recoveryThreshold = null)
         {
             return \Illuminate\Console\Scheduling\Event::sentryMonitor($monitorSlug, $checkInMargin, $maxRuntime, $updateMonitorConfig, $failureIssueThreshold, $recoveryThreshold);
+        }
+
+            }
+    /**
+     * 
+     *
+     */
+    class CallbackEvent {
+        /**
+         * 
+         *
+         * @see \Sentry\Laravel\Features\ConsoleSchedulingIntegration::register()
+         * @param string|null $monitorSlug
+         * @param int|null $checkInMargin
+         * @param int|null $maxRuntime
+         * @param bool $updateMonitorConfig
+         * @param int|null $failureIssueThreshold
+         * @param int|null $recoveryThreshold
+         * @static 
+         */
+        public static function sentryMonitor($monitorSlug = null, $checkInMargin = null, $maxRuntime = null, $updateMonitorConfig = true, $failureIssueThreshold = null, $recoveryThreshold = null)
+        {
+            return \Illuminate\Console\Scheduling\CallbackEvent::sentryMonitor($monitorSlug, $checkInMargin, $maxRuntime, $updateMonitorConfig, $failureIssueThreshold, $recoveryThreshold);
         }
 
             }
