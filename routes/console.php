b830e207d2056122fb69eac03c020a203df44fe3
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-06-27 13:35:28 +0300

    удалена тестовая комманда

--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,3 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Schedule::job(new DeleteUnpaidOrders())->everyMinute();
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
