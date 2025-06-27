9ccb44ab834286d613376443e209d286d6946df2
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-06-27 14:16:35 +0300

    Удалён массив случайных изображений для предварительного просмотра.

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -19,24 +19,10 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
-        $randomImages =[
-            'https://m.media-amazon.com/images/I/41WpqIvJWRL._AC_UY436_QL65_.jpg',
-            'https://m.media-amazon.com/images/I/61ghDjhS8vL._AC_UY436_QL65_.jpg',
-            'https://m.media-amazon.com/images/I/61c1QC4lF-L._AC_UY436_QL65_.jpg',
-            'https://m.media-amazon.com/images/I/710VzyXGVsL._AC_UY436_QL65_.jpg',
-            'https://m.media-amazon.com/images/I/61EPT-oMLrL._AC_UY436_QL65_.jpg',
-            'https://m.media-amazon.com/images/I/71r3ktfakgL._AC_UY436_QL65_.jpg',
-            'https://m.media-amazon.com/images/I/61CqYq+xwNL._AC_UL640_QL65_.jpg',
-            'https://m.media-amazon.com/images/I/71cVOgvystL._AC_UL640_QL65_.jpg',
-            'https://m.media-amazon.com/images/I/71E+oh38ZqL._AC_UL640_QL65_.jpg',
-            'https://m.media-amazon.com/images/I/61uSHBgUGhL._AC_UL640_QL65_.jpg',
-            'https://m.media-amazon.com/images/I/71nDK2Q8HAL._AC_UL640_QL65_.jpg'
-        ];
-
         return [
             'title' => fake()->words(1, true),
             'description' => fake()->paragraph(1),
-            'preview_image' => $randomImages[rand(0, 10)],
+            'preview_image' => '',
             'category_id' => Category::all()->random()->id,
             'sticker_id' => Sticker::all()->random()->id,
             'price' => fake()->numberBetween(100, 1000),
