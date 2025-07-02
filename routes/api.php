3816b6917acc5f331f96bee5cec82375aa64378d
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-07-02 20:48:10 +0300

    Удаляет маршрут тестирования из API для упрощения кода.

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/test', function (Request $request) {
-    $response = Http::get('https://laravel.com/docs/12.x/http-client');
-    dd($response->body());
-});
